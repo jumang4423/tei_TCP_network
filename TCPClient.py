2a9240c676d480085a667d01ffa012b3174b4138
jumang4423 <catontheskate4423@gmail.com>
2020-11-23 09:20:27 +0900

    englsih

--- TCPClient.py
+++ TCPClient.py
@@ -1,25 +1,19 @@
-# coding: UTF-8
-# クライアントを作成
-
 from socket import *
 
 serverAddress = '127.0.0.1'
 serverPort = 50000
 
-# AF = IPv4 という意味
-# TCP/IP の場合は、SOCK_STREAM を使う
-#ソケットの作成(socket)
+# socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
-# サーバを指定(connect)
+# connect
 clientSocket.connect((serverAddress, serverPort))
 sentence = input('lowercase into uppercase:')
 
-# サーバにメッセージを送る(send)
+# send
 clientSocket.send(sentence.encode())
-# ネットワークのバッファサイズは1024。サーバからの文字列を取得する
 modifiedSentence = clientSocket.recv(1024)
 
 print('From Server: ', modifiedSentence.decode())
-#ソケットをクローズ(close)
+#close
 clientSocket.close()
--- TCPServer.py
+++ TCPServer.py
@@ -1,27 +1,21 @@
-# coding: UTF-8
-# サーバを作成
-
 from socket import *
 
 serverPort = 50000
 serverAddress = '127.0.0.1'
-# AF = IPv4 という意味
-# TCP/IP の場合は、SOCK_STREAM を使う
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-# IPアドレスとポートを指定
+# ip adress and port
 serverSocket.bind((serverAddress, serverPort))
-# 1 接続
+# connect
 serverSocket.listen(1)
 print('The server is ready to receive')
-# connection するまで待つ
+
 while True:
-    # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
     connectionSocket, addr = serverSocket.accept()
-    # データを受け取る
+    # get
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
-    # クライアントにデータを返す
+    # send
     connectionSocket.send(capitalizedSentence.encode())
     print('Data is correctly sent now.')
     connectionSocket.close()
